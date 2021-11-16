65cb2e7a9a2b047c598ddd6859d8d62bf9319b67
andreslondo2021 <andresmedellin215@gmail.com>
2021-11-15 21:19:31 -0500

    Version1

--- js/basico.js
+++ js/basico.js
@@ -1,2 +1,40 @@
 var nombre="carlos";
 console.log(typeof nombre);
+ /*
+3. Ejercicios de JAvascript:Se desea hacer un préstamos donde la tasa anual es del 1.2 %,
+pero cada año del préstamo aumenta en un 0.005% la tasa, se desea calcular el valor del
+préstamo si es un monto de 600000, a 3 años.
+a)¡cual es el valor de la cuota mensual
+b) cual es el valor del pago trimestral
+c) cual es el valor en dos años
+d) cual es el valor a pagar al final
+
+4. se tiene una maquina impresora que por segundo imprime hasta 10 unidades de hojas, si
+se ingresan 1200 unidades cual es el tiempo de respuestas de la impresora*/
+
+var monto=600000;
+var cuotano2=0;
+
+var pagotrimestre=0;
+var pagoanual3=0;
+var tasaano2=1.205;
+var tasaano3=1.21;
+
+var vlpagarfinal=0;
+
+
+
+cuotamensual=pagoanual3/36;
+pagotrimestre=pagoanual3/12;
+cuotano2=monto(monto*tasaano2);
+pagoanual3=monto(monto*tasaano3);
+
+
+
+
+
+
+
+
+
+
