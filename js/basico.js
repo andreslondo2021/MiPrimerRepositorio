420ed8506cc44bf2441441233f248caa6e22caa3
andreslondo2021 <andresmedellin215@gmail.com>
2021-11-17 01:20:01 -0500

    version1

--- js/basico.js
+++ js/basico.js
@@ -7,29 +7,33 @@ préstamo si es un monto de 600000, a 3 años.
 a)¡cual es el valor de la cuota mensual
 b) cual es el valor del pago trimestral
 c) cual es el valor en dos años
-d) cual es el valor a pagar al final
+d) cual es el valor a pagar al final*/
 
-4. se tiene una maquina impresora que por segundo imprime hasta 10 unidades de hojas, si
-se ingresan 1200 unidades cual es el tiempo de respuestas de la impresora*/
+
 
 var monto=600000;
 var cuotano2=0;
-
 var pagotrimestre=0;
 var pagoanual3=0;
 var tasaano2=1.205;
 var tasaano3=1.21;
-
 var vlpagarfinal=0;
-
-
-
+pagoanual3=monto(monto*tasaano3);
 cuotamensual=pagoanual3/36;
 pagotrimestre=pagoanual3/12;
 cuotano2=monto(monto*tasaano2);
-pagoanual3=monto(monto*tasaano3);
+console.log(typeof cuotamensual);
+console.log(typeof pagotrimestre);
+console.log(typeof cuotano2);
+console.log(typeof pagoanual3);
+ /*4. se tiene una maquina impresora que por segundo imprime hasta 10 unidades de hojas, si
+se ingresan 1200 unidades cual es el tiempo de respuestas de la impresora*/
 
-
+var unidadesxsegundo=10;
+var cantidadunidades=1200;
+var tiemporespuesta=0;
+tiemporespuesta=1200/10;
+console.log(typeof tiemporespuesta);
 
 
 
